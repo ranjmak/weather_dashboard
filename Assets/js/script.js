dcc9132bc7f3ba4430adc466468b1f6034653efc
boom <ranjit.makwana@gmail.com>
2023-02-07 00:30:20 +0000

    update readme with notes, screenshot and added comments in code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,13 +2,15 @@ $(document).ready(function () {
   var APIKey = "&appID=f435651d3e7405fd1997e707e75f997c";
   var baseURL = "https://api.openweathermap.org/data/2.5/forecast?";
 
-  var cityData = [];
+  var cityData = []; // main data structure for search city history
 
+  // given a city name, the function fetch locally stored data and if any present, loop through to see if the 
+  // city data is already there. returns -1 if not found or the index of the found city
   function getStoredData(city) {
     fetchStoredData();
     var index = -1;
     for (var i=0; i<cityData.length; i++) {
-      
+
       if (cityData[i].name == city) {
         index = i;
       }
@@ -16,6 +18,7 @@ $(document).ready(function () {
     return index;
   }
 
+  // function that does the fetching, checking and rendering of the search history
   function fetchStoredData() {
     var tempCityData = localStorage.getItem("cityData");
     if (tempCityData) {
@@ -25,10 +28,12 @@ $(document).ready(function () {
         $("#history").append(
                     $("<button>").attr({class: "btn btn-outline btn-xl search-button"}).text(cityData[i].name));
       }
+      // add the clear history button to make it easier for users to clear the search history, specially whilst testing
       $("#history").prepend($("<button>").attr({class: "btn btn-outline btn-xl clear"}).text("clear history"));
     }
   }
 
+  // clear hostory listener - needs to delegate since the search history/clear button may not be rendered yet
   $(document).on("click", ".clear", function(event) {
     event.preventDefault();
     cityData = [];
@@ -39,35 +44,42 @@ $(document).ready(function () {
 
   fetchStoredData();
 
+  // store city data to local storage
   function storeCitiesData() {
     localStorage.setItem("cityData", JSON.stringify(cityData));
   }
 
+  // capitalise the city name - this was from stackoverflow and was the best solution for capitalising the city names
+  // we need this to aide the logic later when comparisons are made between whats typed in and whats in the data retrieved from the api call
   function capitaliseCity(city) {
     return city
       .replace(/(\B)[^ ]*/g, match => (match.toLowerCase()))
       .replace(/^[^ ]/g, match => (match.toUpperCase()));
   }
-  // listener for the search submit button
+  // listener for the search submit button - the trigger for the app to come alive!
   $(document).on("click", ".search-button", function (event) {
     event.preventDefault();
 
-    var city = $("#search-input").val().trim();
+    var city = $("#search-input").val().trim(); // get the city value
     city = capitaliseCity(city);
-    var savedCity = $(event.target).text();
+    var savedCity = $(event.target).text(); // get the text of the button (will be a city name or 'Search')
     if (!city && savedCity != $("#search-button").text()) {
-      city = $(event.target).text();
+      city = $(event.target).text(); // set city name to search history city
     }
     else if (!city) {
-      return;
+      return; // city name has already been set to input value, so just return
     }
     else {
-      $("#search-input").val("");
+      $("#search-input").val(""); // clear the input area
     }
-    var index = getStoredData(city);
-    if (index < 0) {
+    var index = getStoredData(city); // get the stored city data if present
+    if (index < 0) { // main index to control whether new city data is used or city history data is used
       var queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=";
-      $.ajax({
+
+      // api call to get the latitude & longitude coordinates for the given city. push it to the cityData and store it locally
+      // set the index to the last data item in the array and call the get and render function, else just call the get and 
+      // render function with the given index
+      $.ajax({ 
         url: queryURL + city + APIKey,
         method: "GET",
       }).then(function (response) {
@@ -90,6 +102,7 @@ $(document).ready(function () {
     }
   });
 
+  // function gets data from the API call given the right index to city's lat & lon 
   function getAndRenderData(index) {
     queryURL =
       baseURL +
@@ -103,12 +116,12 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      // the temperature (converted from Kelvin)
+      // the temperature (converted from Kelvin), using moment() to format for right day
       var forecast = response.list;
       var temperature = (forecast[0].main.temp - 273.15).toFixed(0);
       var today = moment(forecast[0].dt_txt).format("DD/MM/YYYY");
-      // transfer content to HTML
 
+      // set up HTML for todays data and render
       var weatherImg =
         "<img width='50px' height='50px' src='https://openweathermap.org/img/wn/" +
         forecast[0].weather[0].icon +
@@ -126,9 +139,11 @@ $(document).ready(function () {
       $(".wind").text("Wind: " + forecast[0].wind.speed + " KPH");
       $(".humidity").text("humidity: " + forecast[0].main.humidity + "%");
 
+      // now set up html for the forecast area
       $("#forecast").empty();
       $("#forecastTitle").html("<h2>5 Day Forecast</h2>");
       
+      //loop through the data returned from the api call and add the relevant data to forecast section
       for (var i in forecast) {
         var date = moment(forecast[i].dt_txt);
 
